b3ee582d147e2a553cd32d035a74c5da07c9a892
user.emial <emil.mehraliyev003@gmail.com>
2025-06-25 22:33:45 +0300

    Fetch request and security for dashboard was added to core

--- front-end/web/src/pages/Login.jsx
+++ front-end/web/src/pages/Login.jsx
@@ -1,5 +1,45 @@
-function Login() {
-    return <h2>Login page</h2>;
-}
+import { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
+function Login(){
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = e => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:3000/login',{
+                method: 'POST',
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(form)
+            });
+
+            const data = await res.json();
+
+            if(!res.ok) throw new Error (data.error || 'Login failed');
+            localStorage.setItem('token', data.token);
+            navigate('/dashboard');
+        } catch (error){
+            alert('Access error');
+            console.log(error);
+        }
+    };
+    return (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
+            <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      );
+}
 export default Login;
--- front-end/web/src/pages/Register.jsx
+++ front-end/web/src/pages/Register.jsx
@@ -1,5 +1,47 @@
-function Register () {
-    return <h2>Register Page</h2>
+import { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Register(){
+   const navigate = useNavigate();
+   const [form, setForm] = useState({
+     name: '',
+     email: '',
+     password: '',
+   });
+
+   const handleChange = e => {
+    setForm({...form, [e.target.name]: e.target.value});
+   };
+
+   const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+        const res = await fetch('http://localhost:3000/register', {
+          method: 'POST',
+          header: {'Content-Type': 'application/json'},
+          body: JSON.stringify(form)
+        });
+        if(!res.ok) throw new Error('Registration failed');
+
+        alert('Registered successfully')
+        navigate('/login');
+    } catch(error){
+        alert('Registration error');
+        console.log(error);
+    }
+};
+    return(
+        <div>
+            <h2>Register</h2>
+            <form onSubmit ={handleSubmit}>
+                <input name = "name" placeholder = "Name" onChange={handleChange}required/>
+                <input email = "name" type = "email" placeholder = "Email" onChange={handleChange}required/>
+                <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+               <button type = "submit">Register</button>
+            </form>
+        </div>
+    );
+
 }
 
 export default Register;
