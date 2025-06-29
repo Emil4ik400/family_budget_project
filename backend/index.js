049e903d71ed3b1e902f4d37a0188b9d792816a2
user.emial <emil.mehraliyev003@gmail.com>
2025-06-29 22:02:37 +0300

    Cors was added to Index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const expensesRoutes = require('./routes/expenses');
 const incomesRoutes = require('./routes/incomes');
 const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
-
+app.use(cors({
+  origin:'http://localhost:5173',
+  credentials: true
+}))
 app.use(express.json());
 app.use('/api/expenses', expensesRoutes);
 app.use('/api/incomes', incomesRoutes);
